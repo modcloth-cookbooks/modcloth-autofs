62c6d99701d31c0bbd383d894ee27c823430b131
Dan Buch <d.buch@modcloth.com>
2013-10-07 15:23:12 -0400

    Smashing with the modernization hammer
    Closes #2

--- recipes/smartos.rb
+++ recipes/smartos.rb
@@ -1,10 +1,29 @@
+# vim:fileencoding=utf-8
 #
 # Cookbook Name:: autofs
 # Recipe:: smartos
 #
-# Copyright ModCloth, Inc.
+# Copyright (c) 2013 ModCloth, Inc.
 #
-# All rights reserved - Do Not Redistribute
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #
 
 service 'svc:/network/rpc/bind:default' do
@@ -24,8 +43,7 @@ service 'autofs' do
   action [ :enable ]
 end
 
-cookbook_file '/etc/auto_master' do 
+cookbook_file '/etc/auto_master' do
   source 'auto_master'
-  notifies :restart, resources(:service => 'autofs'), :immediately
+  notifies :restart, 'service[autofs]', :immediately
 end
-
